8921856e8ee23eaba54819a97f83eed1dff728aa
hardingCheng <hardingCheng@163.com>
2021-04-15 11:30:19 +0800

    add svg 数字滚动组件 全局样式配置 全局样式函数

---
+++ stsc_frontend/src/components/ScrollingNumbers.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="scrolling-numbers">
+    <countTo :startVal='countObject.startVal' :endVal='countObject.endVal' :duration='3000' :autoplay="countObject.autoplay"></countTo>
+  </div>
+</template>
+
+<script>
+import countTo from 'vue-count-to';
+export default {
+  name: "ScrollingNumbers",
+  components: {
+    countTo
+  },
+  data () {
+    return {
+      countObject:{
+        startVal: 0, // 想开始的价值
+        endVal: 2017, // 达到的价值
+        duration:3000,// 持续时间（毫秒）
+        autoplay:true,// 自动播放时
+        separator:',',//数字分隔 ，
+        prefix:'',// 前缀
+        suffix:''// 后缀
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- stsc_frontend/src/main.js
+++ stsc_frontend/src/main.js
@@ -5,8 +5,15 @@ import store from "./store";
 import "element-ui/lib/theme-chalk/index.css";
 import element from "./tools/elementui";
 import axios from "./api/apiList";
+import VueLazyload from 'vue-lazyload'
 
 Vue.use(element);
+Vue.use(VueLazyload, {
+  preLoad: 1.3,// 预加载高度比例
+  error: '/assets/audio.svg',// 图片路径错误时加载图片   未来是ui给设计
+  loading: '/assets/audio.svg',
+  attempt: 1
+})
 Vue.config.productionTip = false;
 Vue.prototype.$axios = axios;
 new Vue({
