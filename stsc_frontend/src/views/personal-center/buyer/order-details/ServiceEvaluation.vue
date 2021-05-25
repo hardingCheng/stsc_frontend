8cbf2f566418361c98e6e64317ef28bba7874271
Xstar-coder <1390805448@qq.com>
2021-05-25 15:36:36 +0800

    服务评价

--- stsc_frontend/src/views/personal-center/buyer/order-details/ServiceEvaluation.vue
+++ stsc_frontend/src/views/personal-center/buyer/order-details/ServiceEvaluation.vue
@@ -1,6 +1,27 @@
 <template>
   <div class="service-evaluation">
-    ServiceEvaluation
+    <div class="serve_company">
+      <img src="http://p1-q.mafengwo.net/s12/M00/C4/A1/wKgED1vB9ZCABuKMADUYcGrLRP871.jpeg?imageMogr2%2Fthumbnail%2F%21690x450r%2Fgravity%2FCenter%2Fcrop%2F%21690x450%2Fquality%2F90%7Cwatermark%2F1%2Fimage%2FaHR0cDovL2IxLXEubWFmZW5nd28ubmV0L3MxMS9NMDAvNDQvOUIvd0tnQkVGc1A1UnlBRHY3cEFBQUhaWlVQUmxROTkwLnBuZw%3D%3D%2Fgravity%2FSouthEast%2Fdx%2F10%2Fdy%2F11"  class="img_style">
+      <div class="company_detail">
+        <ul>
+          <li>
+            服务商名称:<span>xxx</span>
+          </li>
+          <li>
+            所属机构:<span>xxx</span>
+          </li>
+          <li>
+            联系方式:<span>xxx</span>
+          </li>
+          <li>
+            地址:<span>xxx</span>
+          </li>
+        </ul>
+
+      </div>
+    </div>
+
+    <div class="serve_order"></div>
   </div>
 </template>
 
@@ -10,6 +31,35 @@ export default {
 }
 </script>
 
-<style scoped>
+<style scoped lang="scss">
+.service-evaluation{
+  .serve_company{
+    height: 150px;
+
+    background: pink;
+    .img_style{
+      height: 130px;
+      width: 130px;
+      float: left;
+      margin-top: 10px;
+      margin-left: 10px;
+    }
+    .company_detail{
+      height: 130px;
+      margin-top: 10px;
+      margin-left: 20px;
+      background: #42b983;
+      float: left;
+      ul>li{
+        line-height: 30px;
+      }
+    }
+  }
+  .serve_order{
+    height: 500px;
+    margin-top: 10px;
+    background: #7caced;
+  }
+}
 
 </style>
